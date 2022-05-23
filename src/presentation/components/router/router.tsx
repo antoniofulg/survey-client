16baf47f59a37ecdf808ce160967dd4ca808f04e
Antonio <antoniofulg@gmail.com>
2022-05-23 12:23:56 -0300

    refactor: move ValidationSpy to its own file

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,19 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Login } from '@/presentation/pages'
-import { Validation } from '@/presentation/protocols/validation'
-
-class ValidationSpy implements Validation {
-  errorMessage: string
-  fieldName: string
-  fieldValue: string
-
-  validate(fieldName: string, fieldValue: string): string {
-    this.fieldName = fieldName
-    this.fieldValue = fieldValue
-    return this.errorMessage
-  }
-}
+import { ValidationSpy } from '@/presentation/test'
 
 const validationSpy = new ValidationSpy()
 
