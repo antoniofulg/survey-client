f1e6cd0d29d1d7da7fc4fdbfd78c2f92d850f6c3
Antonio <antoniofulg@gmail.com>
2022-05-24 23:03:08 -0300

    feat: ensure Login calls Authentication with correct values

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -2,14 +2,36 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Login } from '@/presentation/pages'
 import { ValidationStub } from '@/presentation/test'
+import { Authentication, AuthenticationParams } from '@/domain/usecases'
+import { AccountModel } from '@/domain/models'
+import { mockAccountModel } from '@/domain/test'
+
+class AuthenticationSpy implements Authentication {
+  account = mockAccountModel()
+  params: AuthenticationParams
+
+  auth(params: AuthenticationParams): Promise<AccountModel> {
+    this.params = params
+    return Promise.resolve(this.account)
+  }
+}
 
 const validationSpy = new ValidationStub()
+const authenticationSpy = new AuthenticationSpy()
 
 const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login validation={validationSpy} />} />
+        <Route
+          path="/login"
+          element={
+            <Login
+              validation={validationSpy}
+              authentication={authenticationSpy}
+            />
+          }
+        />
       </Routes>
     </BrowserRouter>
   )
