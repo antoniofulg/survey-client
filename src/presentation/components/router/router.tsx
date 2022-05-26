f436971b2ea00b8d6894a55656bf6380b3d0930e
Antonio <antoniofulg@gmail.com>
2022-05-26 18:35:32 -0300

    feat: add Login composition

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,24 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Login } from '@/presentation/pages'
-import { AuthenticationSpy, ValidationStub } from '@/presentation/test'
 
-const validationSpy = new ValidationStub()
-const authenticationSpy = new AuthenticationSpy()
+type Props = {
+  MakeLogin: React.FC
+}
 
-const Router: React.FC = () => {
+const Router: React.FC<Props> = ({ MakeLogin }: Props) => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/login"
-          element={
-            <Login
-              validation={validationSpy}
-              authentication={authenticationSpy}
-            />
-          }
-        />
+        <Route path="/login" element={<MakeLogin />} />
       </Routes>
     </BrowserRouter>
   )
