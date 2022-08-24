903958ad595ff4c06b7787835545887c22abc451
Antonio <antoniofulg@gmail.com>
2022-08-23 23:04:24 -0300

    feat: ensure SignUp change name status to error if validation fails

--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,4 +1,5 @@
 import { SignUp } from '@/presentation/pages/'
+import { ValidationStub } from '@/presentation/test'
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
@@ -6,12 +7,17 @@ type Props = {
   MakeLogin: React.FC
 }
 
+const validationStub = new ValidationStub()
+
 const Router: React.FC<Props> = ({ MakeLogin }: Props) => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<MakeLogin />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route
+          path="/signup"
+          element={<SignUp validation={validationStub} />}
+        />
       </Routes>
     </BrowserRouter>
   )
